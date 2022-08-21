631569f4074222a1ba65b8e572e999f2a2416e5d
gimjinhyeon <canzoo@imweb.me>
2022-08-21 19:15:53 +0900

    Commit before yarn eject

---
+++ styling-react/src/components/SassComponent.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../styles/SassComponent.scss';
+
+const SassComponent = () => {
+	return (
+		<div className="SassComponent">
+			<div className="box red">red</div>
+			<div className="box orange">orange</div>
+			<div className="box yellow">yellow</div>
+			<div className="box green">green</div>
+			<div className="box blue">blue</div>
+			<div className="box indigo">indigo</div>
+			<div className="box violet">violet</div>
+		</div>
+	)
+}
+
+export default SassComponent;
