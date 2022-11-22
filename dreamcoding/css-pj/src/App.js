10c45d8332bbe075d8bb550e9773969d1a119752
gimjinhyeon <canzoo@imweb.me>
2022-11-22 18:48:09 +0900

    tailwind component

--- dreamcoding/css-pj/src/App.js
+++ dreamcoding/css-pj/src/App.js
@@ -1,36 +1,17 @@
 import './App.css';
 import Button1 from './components/Button1';
 import Button2 from './components/Button2';
-import styled, {css} from 'styled-components';
+import StyledComponents from './components/StyledComponents';
+import TailwindComponent from './components/TailwindComponent';
 
-const Container = styled.div`
-  display : flex;
-`;
-const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid #3c5b69;
-  color: #b9eaff;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-  ${(props) => 
-    props.primary &&
-    css`
-      background: #009cd5;
-      color: white;
-    `
-  }
-`;
 
 function App() {
   return (
     <div className="App">
       <Button1></Button1>
       <Button2></Button2>
-      <Container>
-        <Button>normal Button</Button>
-        <Button primary>primary Button</Button>
-      </Container>
+      <StyledComponents></StyledComponents>
+      <TailwindComponent></TailwindComponent>
     </div>
   );
 }
